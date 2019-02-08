f752b7464127734269597a5c80a3e5f189374e84
Anuar Harb <anuarharb@hotmail.com>
2019-02-07 21:31:02 -0600

    Cree una vaca pirata con cowsay

---
+++ index.js
@@ -0,0 +1,11 @@
+var cowsay = require('cowsay');
+
+console.log(cowsay.say({
+  text : "SoY UNA VAQUITA PIRATA",
+  e : "x-o",
+  T : "v "
+}));
+
+console.log('hola');
+console.log('Soy un archivo de Js');
+console.log(8+9);
