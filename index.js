1b3bb30b21b92f51bee2ae4a550383160fc5b85d
Anuar Harb <anuarharb@hotmail.com>
2019-02-07 21:54:11 -0600

    historia de vaquita

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var cowsay = require('cowsay');
 
+let nombre = "Anuar";
+let fraceVaquita = "Surcaré los 7 mares de pasto";
+
+let historia = "Hola, mi nombre es " + nombre + ". Y hoy me enconté con una vaquita pitara que decía: " + fraceVaquita;
+
+console.log(historia);
+
 console.log(cowsay.say({
-  text : "SoY UNA VAQUITA PIRATA",
+  text : fraceVaquita,
   e : "x-o",
   T : "v "
 }));
-
-console.log('hola');
-console.log('Soy un archivo de Js');
-console.log(8+9);
