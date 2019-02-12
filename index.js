842d78303735a14397646584c461016c85dd216e
Effv2 <frentyv2.0@gmail.com>
2019-02-11 21:19:25 -0600

    Final equipo

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const cowsay = require('cowsay');
+const cowsay = require("cowsay");
 const intro = require('./intro');
 const nudo = require('./nudo');
 const fin = require('./fin');
@@ -8,7 +8,7 @@ const equipo = "Astros";
 const fraceVaquita = "hola";
 const planeta = "Marte";
 const sospechoso = "MundoTerrestre";
-const emocion = "feliz como una lombriz";
+const emocion = "feliz como una lombriz, bien happy ";
 var edad = Math.floor(Math.random() * 100);
 let historia = `${nombre} es un marciano que tiene ${edad}`
 
@@ -16,7 +16,7 @@ console.log(historia);
 
 console.log(intro.historia(nombre, equipo,edad));
 console.log(nudo.historia(equipo, planeta, sospechoso));
-console.log(fin.historia(equipo, emocion));
+console.log(fin.historia(edad, equipo, emocion));
 
 
 console.log(cowsay.say({
