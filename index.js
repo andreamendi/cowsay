917f51f8f0be280b2126a89f2b01e6797424e086
Effv2 <frentyv2.0@gmail.com>
2019-02-12 21:58:39 -0600

    Commit con funciones y callback

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
 const cowsay = require("cowsay");
 const intro = require('./intro');
-<<<<<<< HEAD
-const nudo = require('./nudo');
-const nudo2 = require('./nudo2');
-=======
 const nudo1 = require('./nudo1');
->>>>>>> 44e5b66eea3bd6abb86887bd1d83585ef0a3a49c
+const nudo2 = require('./nudo2');
 const fin = require('./fin');
+const fs = require('fs');  //filesystem
 
+//funcionPrincipal
+fs.readFile('nombres.txt', 'utf-8', function(err, contenido){
+	if(err) {    	//En esta linea cachamos el error
+		throw err;
+	}
+
+	const nombres = contenido.split(',');   //funcion split para separar
+	const nombreran = nombres[Math.floor(Math.random() * nombres.length)];
+	const historiacompleta = juntarHistoria(nombreran);
+	console.log(historiacompleta);
+
+	fs.writeFile('historia.txt', historiacompleta, function(err){
+		if(err){
+			throw err;
+		}
+	})
+
+	console.log(nombres.length);	//console.log   es para mandar a pantalla
+	console.log(nombreran);  		// Si no hay error, se ejecuta esto <-
+});
+
+//Muestra la historia
+
+function juntarHistoria(nombre){
+	const inici = (intro.historia(nombre, equipo,edad,planeta));
+  const nod = (nudo1.historia(ciudad, costo, edad_espectadores, visit, local));
+  const nod1 = (nudo2.historia(edad,equipo, planeta, sospechoso));
+	const finall =(fin.historia(edad, equipo, emocion));
+	const historiacompleta = inici + nod + nod1 + finall;
+	return historiacompleta;
+	
+}
+
+//Variables
 const edad_espectadores = 11;
+const equipo = "Diablos";
+;const planeta = "Tierra";
 const ciudad = "Toronto";
 const costo = "$5";
 const fraceVaquita = "hola";
@@ -17,11 +50,6 @@ var local = "local";
 const sospechoso = "MundoTerrestre";
 const emocion = "feliz como una lombriz, bien happy ";
 var edad = Math.floor(Math.random() * 100);
-
-console.log(intro.historia(nombre, equipo,edad,planeta));
-console.log(nudo1.historia(ciudad, costo, edad_espectadores, visit, local));
-console.log(nudo2.historia(edad,equipo, planeta, sospechoso));
-console.log(fin.historia(edad, equipo, emocion));
 
 console.log(cowsay.say({
   text : fraceVaquita,
