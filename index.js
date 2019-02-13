018982b626455d83ea4838b503774e64c92fe23b
AndreaMendi <andreamendi@icloud.com>
2019-02-12 21:38:35 -0600

    ya estan listas las funciones de personajes y del equipo random ambos y quedo bien

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+//modulos
 const cowsay = require("cowsay");
 const intro = require('./intro');
 const nudo1 = require('./nudo1');
 const nudo2 = require('./nudo2');
 const fs = require('fs');
 const fin = require('./fin');
+
+//variables globales
 const ciudad = "Toronto";
 const nombre = "Git";
 const costo = "$5";
@@ -16,15 +19,61 @@ const sospechoso = "MundoTerrestre";
 const emocion = "feliz como una lombriz, bien happy ";
 var edad = Math.floor(Math.random() * 100);
 const edad_espectadores = edad;
-const introHistoria = intro.historia(nombre, equipo,edad,planeta);
-const introNudoUno = nudo1.historia(ciudad, costo, edad_espectadores, visit, local);
-const introNudoDos = nudo2.historia(edad,equipo, planeta, sospechoso);
-const introFin = fin.historia(edad, equipo, emocion);
 
-const historiaCompleta = introHistoria + introNudoUno + introNudoDos + introFin;
+function selectPersonaje(){
+  fs.readFile('personajes.txt','utf-8', function(err, contenido){
+    if(err){
+      //THROW Detecta algo y lo manda tipo return
+      throw err;
+    }
+  
+    const personajes = contenido.split(','); //Se está separando el archivo txt por comas y lo vuelve un array
+    const personajesNumero = Math.floor(Math.random() * personajes.length); // Se hace un número random entre el length del txt.
+    const personajesRandom = personajes[personajesNumero]; // Se imprime el personaje que está en la posición random del punto de arriba.
+   
+    selectEquipo(personajesRandom)
+  
+
+  
+    // const historiaCompleta= juntarHistoria(personajesRandom); //Se ejecuta con esté personaje
+    // console.log(historiaCompleta);
+    // console.log(personajes.length);
+    // console.log(personajesRandom);
+    
+  });
+}
 
 
-console.log(historiaCompleta);
+function selectEquipo(personajesRandom){
+  fs.readFile('equipo.txt','utf-8', function(err, contenido){
+
+    if(err){
+      //THROW Detecta algo y lo manda tipo return
+      throw err;
+    }
+  
+    const equipo = contenido.split(',');
+    const numeroEquipo = Math.floor(Math.random() * equipo.length);
+    const randomEquipo = equipo[numeroEquipo];
+  
+    juntarHistoria(personajesRandom, randomEquipo)
+  });
+}
+
+
+selectPersonaje()
+
+//Se ejecuta la historia
+
+function juntarHistoria(randomPersonaje, randomEquipo){
+  const introHistoria = intro.historia(randomPersonaje,randomEquipo,edad,planeta);
+  const introNudoUno = nudo1.historia(ciudad, costo, edad_espectadores, visit, local);
+  const introNudoDos = nudo2.historia(edad,randomEquipo, planeta, sospechoso);
+  const introFin = fin.historia(edad, randomEquipo, emocion);
+  const historiaCompleta = introHistoria + introNudoUno + introNudoDos + introFin;
+  console.log(historiaCompleta);
+}
+
 
 console.log(cowsay.say({
   text : fraceVaquita,
