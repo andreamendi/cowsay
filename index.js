2c5f0de46551fb996cd475ab4bf1df61fe18c24a
C Mendi <cesarmendezbeltran@yahoo.com.mx>
2019-02-12 20:03:21 -0600

    es mi rama

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-const cowsay = require("cowsay");
+const cowsay = require('cowsay');
 const intro = require('./intro');
-const nudo1 = require('./nudo1');
+const fs = require ('fs');
+
+//const nudo = require('./nudo');
 const nudo2 = require('./nudo2');
+
+const nudo1 = require('./nudo1');
+
 const fin = require('./fin');
+
+const edad_espectadores = 11;
 const ciudad = "Toronto";
-const nombre = "Git";
 const costo = "$5";
 const fraceVaquita = "hola";
 var visit = "visit";
 var local = "local";
-const equipo = "Diablos";
-const planeta = "Marte"; 
 const sospechoso = "MundoTerrestre";
-const emocion = "feliz como una lombriz, bien happy ";
-var edad = Math.floor(Math.random() * 100);
-const edad_espectadores = edad;
+const emocion = "feliz como una lombriz";
+var edad = Math.floor(Math.random()*100);
 
+const introHistoria = intro.historia(nombre, equipo,edad,planeta);
+const historiaCompleta = introHistoria+ finalHistoria;
 
-console.log(intro.historia(nombre, equipo,edad,planeta));
+console.log(introHistoria);
 console.log(nudo1.historia(ciudad, costo, edad_espectadores, visit, local));
 console.log(nudo2.historia(edad,equipo, planeta, sospechoso));
-console.log(fin.historia(edad, equipo, emocion));
-
-
-
-
+console.log(fin.historia(equipo, emocion));
 
 
 console.log(cowsay.say({
