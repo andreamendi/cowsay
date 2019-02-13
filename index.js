0154de42372b12d45395cd88e56e26dceb959db2
AndreaMendi <andreamendi@icloud.com>
2019-02-12 20:00:56 -0600

    Branch de Andrea

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const cowsay = require("cowsay");
 const intro = require('./intro');
 const nudo1 = require('./nudo1');
 const nudo2 = require('./nudo2');
+const fs = require('fs');
 const fin = require('./fin');
 const ciudad = "Toronto";
 const nombre = "Git";
@@ -15,17 +16,15 @@ const sospechoso = "MundoTerrestre";
 const emocion = "feliz como una lombriz, bien happy ";
 var edad = Math.floor(Math.random() * 100);
 const edad_espectadores = edad;
+const introHistoria = intro.historia(nombre, equipo,edad,planeta);
+const introNudoUno = nudo1.historia(ciudad, costo, edad_espectadores, visit, local);
+const introNudoDos = nudo2.historia(edad,equipo, planeta, sospechoso);
+const introFin = fin.historia(edad, equipo, emocion);
+
+const historiaCompleta = introHistoria + introNudoUno + introNudoDos + introFin;
 
 
-console.log(intro.historia(nombre, equipo,edad,planeta));
-console.log(nudo1.historia(ciudad, costo, edad_espectadores, visit, local));
-console.log(nudo2.historia(edad,equipo, planeta, sospechoso));
-console.log(fin.historia(edad, equipo, emocion));
-
-
-
-
-
+console.log(historiaCompleta);
 
 console.log(cowsay.say({
   text : fraceVaquita,
